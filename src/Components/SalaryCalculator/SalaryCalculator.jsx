7a1a7afd450863a31bace085b659f58d642832e9
Lewis Sneddon <>
2021-02-27 11:59:51 +0000

    started wiring up basic components and added prettier formatting

---
+++ src/Components/SalaryCalculator/SalaryCalculator.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CollectInputs from "./CollectInputs/CollectInputs";
+
+function SalaryCalculator() {
+  return (
+    <div>
+      <h3>SalaryCalculator</h3>
+      <CollectInputs />
+    </div>
+  );
+}
+
+export default SalaryCalculator;
