453c621cc7d7af62a9c839739e0db9f232d90d86
Lewis Sneddon <>
2021-02-27 12:05:44 +0000

    basic skeleton in place

---
+++ src/Components/SalaryCalculator/DisplayOutputs/DisplayOutputs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function DisplayOutputs() {
+  return (
+    <div>
+      <p>Monthly take home: </p>
+      <p>Monthly income tax: </p>
+      <p>Monthly NI contributions: </p>
+    </div>
+  );
+}
+
+export default DisplayOutputs;
--- src/Components/SalaryCalculator/SalaryCalculator.jsx
+++ src/Components/SalaryCalculator/SalaryCalculator.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CollectInputs from "./CollectInputs/CollectInputs";
+import DisplayOutputs from './DisplayOutputs/DisplayOutputs';
 
 function SalaryCalculator() {
   return (
     <div>
       <h3>SalaryCalculator</h3>
       <CollectInputs />
+      <DisplayOutputs />
     </div>
   );
 }
